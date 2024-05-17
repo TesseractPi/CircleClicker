a56acc5ddd97bc49b56a50e9ff10ad462a5213cb
TesseractPi <149723729+TesseractPi@users.noreply.github.com>
2024-05-17 15:07:03 -0700

    Add a bash port [WIP]

---
+++ CircleClicker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#   ____ _          _         ____                     _                __  __            _     _
+#  / ___(_)_ __ ___| | ___   |  _ \ _ __ __ ___      _(_)_ __   __ _   |  \/  | __ _  ___| |__ (_)_ __   ___
+# | |   | | '__/ __| |/ _ \  | | | | '__/ _` \ \ /\ / / | '_ \ / _` |  | |\/| |/ _` |/ __| '_ \| | '_ \ / _ \
+# | |___| | | | (__| |  __/  | |_| | | | (_| |\ V  V /| | | | | (_| |  | |  | | (_| | (__| | | | | | | |  __/
+#  \____|_|_|  \___|_|\___|  |____/|_|  \__,_| \_/\_/ |_|_| |_|\__, |  |_|  |_|\__,_|\___|_| |_|_|_| |_|\___|
+#                                                              |___/ 
+# By Sam Rohrbach (github.com/TesseractPi)
+# Built for Neal.fun's perfect circle test at https://neal.fun/perfect-circle/
+# This should be on my website (https://samrohrbach.space)
+# In its default state, this is built for a 1366x768 screen in fullscreen. Change the values accordingly.
+
+center_x=683.5
+center_y=364.5
+radius=256
+
+angle_increment=6
+angle_repeat=$((  ))
+
+getXY ()
+{
+	x=$(( $radius *  ))
+}
+
+
