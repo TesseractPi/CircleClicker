d1c701a54a395cc16b4ccdc4bff0ccb8df302316
TesseractPi <149723729+TesseractPi@users.noreply.github.com>
2024-05-21 14:49:13 -0700

    Make more progress on the bash script

--- CircleClicker.sh
+++ CircleClicker.sh
@@ -10,16 +10,40 @@
 # This should be on my website (https://samrohrbach.space)
 # In its default state, this is built for a 1366x768 screen in fullscreen. Change the values accordingly.
 
-center_x=683.5
-center_y=364.5
-radius=256
+pi=$(seq -f '4/%g' 1 2 99999 | paste -sd-+ | bc -l) # because there's no built-in pi
 
-angle_increment=6
-angle_repeat=$((  ))
+center_x=683.5 # center x of circle
+center_y=364.5 # center y of circle
+radius=256 # radius of circle
+new_center_y=$(( $center_y + $radius ))
 
-getXY ()
+angle_increment=6 # must be divisible by 360 AND be less than 180, ***CHANGE THIS VARIABLE!***
+angle_repeat=$(( ( 360 / $angle_increment ) + 1 ))
+angle=0 # starting angle, DO NOT CHANGE
+
+function getXY ()
 {
-	x=$(( $radius *  ))
+	x=$(( ( $radius * sin($pi * 2 * $angle / 360) ) + $center_x ))
+	y=$(( ( $radius * sin($pi * 2 * $angle / 360) ) + $center_y ))
+	echo "($x, $y)"
 }
 
+clear
+echo "Press A to draw a circle or press Q to quit"
 
+while true
+do
+	read -rsn1 isPressed_A
+	read -rsn1 isPressed_Q
+
+	if [ $isPressed_Q = "q" ]
+	then
+		echo "Goodbye!"
+		break
+	fi
+
+	if [ $isPressed_A = "a" ] 
+	then
+		xdotool mousemove $center_x $new_center_y
+fi
+done
